26e639e97e0cbb99a0151fe564a52c2b6358cb5b
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-11 21:39:59 +0100

    feat(cli): basic pandoc support
    added support for converting between a few text formats supported by pandoc

--- convert.sh
+++ convert.sh
@@ -6,14 +6,25 @@ image/webp
 image/png
 video/webm
 video/mp4
+text/markdown
+text/tex
+text/org
+text/html
+text/pdf
+text/docx
 "
 
 selected=$(echo "$formats" | fzf)
 format=$(echo "$selected" | sed 's/.\+\/\(.\+\)/\1/')
+category=$(echo "$selected" | sed 's/\(.\+\)\/.\+/\1/')
 
 for arg in "$@"
 do
     # shellcheck disable=SC2001
     name=$(echo "$arg" | sed 's/\(.\+\)\..\+/\1/')
-    ffmpeg -i "$arg" "${name}.${format}"
+    if [ "$category" = "text" ]; then
+        pandoc -s "$arg" -o "${name}.${format}"
+    else
+        ffmpeg -i "$arg" "${name}.${format}"
+    fi
 done
