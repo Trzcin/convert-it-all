b92eaf77c011ed694d0fe308369a0b7db7144943
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-12 20:18:15 +0100

    fix(cli): show only the extension in fzf

--- convert.sh
+++ convert.sh
@@ -18,22 +18,22 @@ ext=$(echo "$1" | cut -d '.' -f 2)
 input_category=$(echo "$formats" | grep "$ext" | cut -d '/' -f 1)
 matching_formats=$(echo "$formats" | grep "$input_category")
 matching_formats=$(echo "$matching_formats" | grep -v "$ext")
+matching_formats=$(echo "$matching_formats" | cut -d '/' -f 2)
 
 selected=$(echo "$matching_formats" | fzf)
 # shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     exit
 fi
-category=$(echo "$selected" | cut -d '/' -f 1)
-format=$(echo "$selected" | cut -d '/' -f 2)
+category=$(echo "$formats" | grep "/$selected" | cut -d '/' -f 1)
 
 for arg in "$@"
 do
     name=$(echo "$arg" | cut -d '.' -f 1)
 
     if [ "$category" = "text" ]; then
-        pandoc -s "$arg" -o "${name}.${format}"
+        pandoc -s "$arg" -o "${name}.${selected}"
     else
-        ffmpeg -i "$arg" "${name}.${format}"
+        ffmpeg -i "$arg" "${name}.${selected}"
     fi
 done
