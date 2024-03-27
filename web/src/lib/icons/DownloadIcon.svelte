520e57d1f784075dfacb4a242cf0b4bc12aaf47e
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-27 23:06:20 +0100

    feat(web): add icons as components
    additionally added an alias for src/lib: $lib

---
+++ web/src/lib/icons/DownloadIcon.svelte
@@ -0,0 +1,16 @@
+<svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M2.6665 11.3334V12.6667C2.6665 13.0204 2.80698 13.3595 3.05703 13.6096C3.30708 13.8596 3.64622 14.0001 3.99984 14.0001H11.9998C12.3535 14.0001 12.6926 13.8596 12.9426 13.6096C13.1927 13.3595 13.3332 13.0204 13.3332 12.6667V11.3334M4.6665 7.33341L7.99984 10.6667M7.99984 10.6667L11.3332 7.33341M7.99984 10.6667V2.66675"
+        stroke="var(--color, var(--accent))"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
+
+<style>
+    svg {
+        width: var(--size, 16px);
+        height: var(--size, 16px);
+    }
+</style>
---
+++ web/src/lib/icons/UploadIcon.svelte
@@ -0,0 +1,16 @@
+<svg viewBox="0 0 56 59" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M4 43V49C4 50.5913 4.63214 52.1174 5.75736 53.2426C6.88258 54.3679 8.4087 55 10 55H46C47.5913 55 49.1174 54.3679 50.2426 53.2426C51.3679 52.1174 52 50.5913 52 49V43M13 19L28 4M28 4L43 19M28 4V40"
+        stroke="var(--color, var(--gray))"
+        stroke-width="8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
+
+<style>
+    svg {
+        width: var(--size, 56px);
+        height: var(--size, 59px);
+    }
+</style>
