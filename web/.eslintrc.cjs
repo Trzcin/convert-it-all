75b37c866dc8d6bf2587db3f428a53d0edd11c24
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-27 22:24:22 +0100

    feat(web): add prettier

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,43 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:svelte/prettier"
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:svelte/prettier',
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
         },
         {
-            "files": ["*.svelte"],
-            "parser": "svelte-eslint-parser",
-            "parserOptions": {
-                "parser": "@typescript-eslint/parser"
-            }
-        }
+            files: ['*.svelte'],
+            parser: 'svelte-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+            },
+        },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "extraFileExtensions": ['.svelte']
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.svelte'],
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "eqeqeq": "error"
-    }
-}
+    plugins: ['@typescript-eslint'],
+    rules: {
+        eqeqeq: 'error',
+    },
+};
