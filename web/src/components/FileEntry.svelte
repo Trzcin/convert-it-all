e937656674d43d6cfd417ea09099407202c2999a
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-28 22:56:40 +0100

    feat(web): add FileEntry component

---
+++ web/src/components/FileEntry.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import DocumentIcon from '$lib/icons/DocumentIcon.svelte';
+    import ImageIcon from '$lib/icons/ImageIcon.svelte';
+    import VideoIcon from '$lib/icons/VideoIcon.svelte';
+
+    export let file: File;
+
+    const fileSizeFormatter = Intl.NumberFormat('en', {
+        notation: 'compact',
+        style: 'unit',
+        unit: 'byte',
+        unitDisplay: 'narrow',
+    });
+
+    $: type = file.type.split('/')[0];
+</script>
+
+{#if type === 'image'}
+    <ImageIcon />
+{:else if type === 'video'}
+    <VideoIcon />
+{:else}
+    <DocumentIcon />
+{/if}
+<p>{file.name}</p>
+<span>{fileSizeFormatter.format(file.size)}</span>
+
+<style>
+    p {
+        font-weight: 600;
+        margin-left: 0.75rem;
+    }
+
+    span {
+        margin-left: 1rem;
+        font-weight: 400;
+        color: var(--gray);
+    }
+</style>
