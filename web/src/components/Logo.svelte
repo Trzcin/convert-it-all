d10d1440fffabbb811292c0ad4741d53796cc060
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-28 21:43:40 +0100

    feat(web): change Logo appearence depending on appState

--- web/src/components/Logo.svelte
+++ web/src/components/Logo.svelte
@@ -1,9 +1,12 @@
 <script>
     import MainIcon from '$lib/icons/MainIcon.svelte';
+    import appState from '$lib/stores/appState';
+
+    $: displayRow = !$appState.startsWith('no-files');
 </script>
 
-<div>
-    <MainIcon />
+<div class:row={displayRow}>
+    <MainIcon --size={displayRow ? '3rem' : '7.5rem'} />
     <h1>Convert It All</h1>
 </div>
 
@@ -16,8 +19,17 @@
         margin-top: 7.5rem;
     }
 
+    .row {
+        flex-direction: row;
+        margin-top: 2rem;
+    }
+
     h1 {
         font-size: 3rem;
         font-weight: 800;
+    }
+
+    .row h1 {
+        font-size: 2rem;
     }
 </style>
