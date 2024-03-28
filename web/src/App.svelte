7ccb908119db4dde97777b7bba60b3c0e14dc9dd
Mikołaj Trzciński <trzccin@gmail.com>
2024-03-28 19:32:58 +0100

    feat(web): main page done

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+    import UploadIcon from '$lib/icons/UploadIcon.svelte';
+    import Logo from './components/Logo.svelte';
 </script>
 
-<h1>Hello World</h1>
+<Logo />
+<div id="prompt">
+    <UploadIcon />
+    <h2>Click anywhere or drop files here to start</h2>
+</div>
+
+<style>
+    #prompt {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1.5rem;
+        margin-top: 9rem;
+    }
+</style>
---
+++ web/src/components/Logo.svelte
@@ -0,0 +1,23 @@
+<script>
+    import MainIcon from '$lib/icons/MainIcon.svelte';
+</script>
+
+<div>
+    <MainIcon />
+    <h1>Convert It All</h1>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+        margin-top: 7.5rem;
+    }
+
+    h1 {
+        font-size: 3rem;
+        font-weight: 800;
+    }
+</style>
